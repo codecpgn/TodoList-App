be9e344b70c9ddf537ddc79041eabc26a2f0e8fa
surya <cpgnsc@gmail.com>
2022-09-27 23:26:13 +0600

    __|47|20mins|__

--- src/toDolist.jsx
+++ src/toDolist.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './index.css'
 import Button from '@mui/material/Button';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 const Todolist = () => {
+
+const [item,setItem] = useState(false);
+const [newitem, setNewItem] = useState([]);
+const itemEvent = (event) =>{
+setItem(event.target.value);
+}
+
+const listofItems = () =>{
+  setNewItem((prevValue) => {
+return [...prevValue, item];
+  })
+};
+
   return (
     <>
       <div className='main_div'>
@@ -12,10 +25,19 @@ const Todolist = () => {
       <h1>ToDO List</h1>
       <br/>
 
-      <input type='text' placeholder='add an item'/>
-      <Button className='newBtn'>
+      <input type='text' placeholder='add an items' onChange={itemEvent}/>
+      <Button className='newBtn' onClick={listofItems}>
         <AddCircleIcon/>
       </Button>
+      <br/>
+      <ol>
+        
+
+        {newitem.map((val) =>{
+          return <li>{val}</li>;
+        })}
+      </ol>
+      <br/>
       </div>
       </div>
     </>
